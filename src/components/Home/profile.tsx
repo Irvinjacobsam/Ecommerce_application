c75ada6900d4007c077fa2f4a11c9e800cf0db20
Irvin Jacob Sam <irvinjacsa7@gmail.com>
2024-03-01 22:07:15 +0530

    initial commit

---
+++ src/components/Home/profile.tsx
@@ -0,0 +1,15 @@
+import React, { memo, useState } from 'react'
+
+
+function Profile() {
+  
+  return (
+    <div>
+        
+        <h3>Profile</h3>
+        <p> Hi </p>
+    </div>
+  )
+}
+
+export default memo(Profile);
